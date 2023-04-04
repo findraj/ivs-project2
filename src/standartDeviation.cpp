9213eb594eb1845bd9b4a4532c104831c4a5785f
Marko Olesak <markole16@gmail.com>
2023-04-04 17:41:43 +0200

    Created standart deviation program + test files

---
+++ src/standartDeviation.cpp
@@ -0,0 +1,67 @@
+#include <vector>
+#include <stdexcept>
+#include <cstdio>
+#include "mathLib.h"
+
+// result = sqrt[ Σ(xᵢ - x̄)² / (n - 1) ]
+
+// x̄
+double Mean(const std::vector<double> &values)
+{
+    double sum = 0.0;
+    for (double value : values)
+    {
+        sum = Addition(sum, value);
+    }
+
+    double mean = Division(sum, values.size());
+    return mean;
+}
+
+// Σ(xᵢ - x̄)²
+double SumOfSquares(const std::vector<double> &values, double mean)
+{
+    double sum = 0.0;
+    for (double value : values)
+    {
+        double diff = Subtraction(value, mean);
+        double square = Power(diff, 2.0);
+        sum = Addition(sum, square);
+    }
+    return sum;
+}
+
+// Σ(xᵢ - x̄)² / (n - 1)
+double Variance(const std::vector<double> &values)
+{
+    if (values.empty())
+        throw std::invalid_argument("Input vector is empty");
+
+    double meanValue = Mean(values);
+    double sumOfSquaresValue = SumOfSquares(values, meanValue);
+    double variance = Division(sumOfSquaresValue, values.size());
+    return variance;
+}
+
+// sqrt[ Σ(xᵢ - x̄)² / (n - 1) ]
+double StandardDeviation(const std::vector<double> &values)
+{
+    double varianceValue = Variance(values);
+    double standardDeviation = Nthroot(varianceValue, 2.0);
+    return standardDeviation;
+}
+
+int main()
+{
+    std::vector<double> values;
+    double num;
+    while (scanf("%lf", &num) == 1)
+    {
+        values.push_back(num);
+    }
+
+    double sd = StandardDeviation(values);
+    printf("Standard deviation: %lf\n", sd);
+
+    return 0;
+}
